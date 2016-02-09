d9f6e492df75b3d759d72b9fb4b2d49fc249b1c3
w1em <willem@ells-family.com>
2016-02-09 18:03:37 -0500

    this is the first upload of a file that will be used for learning reference

---
+++ learningReference.py
@@ -0,0 +1,12 @@
+#strings
+s = 'furry dog'
+s.rstrip() #remove whitespace
+
+s.split(' ') # split into list where space is
+
+s.isdigit() #check for whether or not it is a number
+
+s.endswith('y') # check last letter of string
+
+s.lower()
+s.upper() # lower/uppercase letters
