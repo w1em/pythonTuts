67022c9de0f26df380f00d724b933fb5cde633bf
w1em <willem@ells-family.com>
2016-02-11 17:51:55 -0500

    third upload of learning reference file with new string addition

--- learningReference.py
+++ learningReference.py
@@ -9,4 +9,10 @@ s.isdigit() #check for whether or not it is an integer
 s.endswith('y') # check last letter of string
 
 s.lower()
-s.upper() # lower/uppercase letters
+s.upper() # lower/uppercase letter
+
+
+multiLineString = '''This string will
+extend multiple
+lines in the
+code''' ## Use triple quotes to make multiline text
